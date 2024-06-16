34c7006c881406d14a8b2cdfc8b0d1543d640280
nitinsatya2520 <nitinsatya6561@gmail.com>
2024-06-16 20:04:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react"; 
+import validator from 'validator'
 
-function App() {
+const App = () => { 
+  
+  const [errorMessage, setErrorMessage] = useState('') 
+
+  const validate = (value) => { 
+
+      if (validator.isStrongPassword(value, { 
+          minLength: 8, minLowercase: 1, 
+          minUppercase: 1, minNumbers: 1, minSymbols: 1 
+      })) { 
+          setErrorMessage('Your Password is Strong') 
+          if (validator.isStrongPassword(value, { 
+            minLength: 8, minLowercase: 2, 
+            minUppercase: 1, minNumbers: 2, minSymbols: 2 
+        })) { 
+            setErrorMessage('Your Password is Super Strong') 
+            
+        }
+      } else { 
+          setErrorMessage('Your Password is week') 
+      } 
+  } 
   return (
     <div className="App">
+       
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         
+            <pre> 
+                <h2>Checking Password Strength</h2> 
+                <span>Enter Password: </span><input type="text"
+                    onChange={(e) => validate(e.target.value)}></input> <br /> 
+                {errorMessage === '' ? null : 
+                    <span style={{ 
+                        fontWeight: 'bold', 
+                        color: 'red', 
+                    }}>{errorMessage}</span>} 
+            </pre> 
+
+            
+            
+          
+
       </header>
+  
+      
     </div>
   );
 }
